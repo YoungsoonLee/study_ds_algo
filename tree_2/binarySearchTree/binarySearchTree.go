592b3f87a2fd24c42c762bac0c8af8a097a9980d
Ted Youngsoon Lee <youngtip@gmail.com>
2020-10-07 00:02:08 -0700

    .

--- tree_2/binarySearchTree/binarySearchTree.go
+++ tree_2/binarySearchTree/binarySearchTree.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type BSTNode struct {
 	data  int
 	left  *BSTNode
@@ -192,4 +196,151 @@ func IsBST(root *BSTNode) bool {
 	}
 
 	return true
+}
+
+func BST2DLL(root *BSTNode) {
+	if root == nil || (root.left == nil && root.right == nil) {
+		return
+	}
+
+	BST2DLL(root.left)
+	BST2DLL(root.right)
+	currRight := root.right
+	root.right = root.left
+	root.left = nil
+	for root.right != nil {
+		root = root.right
+	}
+	root.right = currRight
+}
+
+func SortedArrayToBST(A []int) *BSTNode {
+	if A == nil {
+		return nil
+	}
+
+	return helper(A, 0, len(A)-1)
+}
+
+func helper(A []int, low int, high int) *BSTNode {
+	if low > high {
+		return nil
+	}
+
+	mid := low + (high-low)/2
+	node := new(BSTNode)
+	node.data = A[mid]
+	node.left = helper(A, low, mid-1)
+	node.right = helper(A, mid+1, high)
+	return node
+}
+
+func SplitList(head *ListNode) (*ListNode, *ListNode, *ListNode) {
+	if head.next == nil {
+		return nil, head, nil
+	}
+
+	slowPointer := head
+	fastPointer := head
+	previousPointer := head
+
+	for fastPointer != nil && fastPointer.next != nil {
+		previousPointer = slowPointer
+		fastPointer = fastPointer.next.next
+		slowPointer = slowPointer.next
+	}
+	previousPointer.next = nil
+	return head, slowPointer, slowPointer.next
+}
+
+func SortedListToBST(head *ListNode) *BSTNode {
+	if head == nil {
+		return nil
+	}
+
+	left, middle, right := SplitList(head)
+
+	var node BSTNode
+	node.data = middle.data
+	node.left = SortedListToBST(left)
+	node.right = SortedListToBST(right)
+	return &node
+}
+
+func kthSmallest(root *BSTNode, k int) *BSTNode {
+	counter := 0
+	return helper2(root, k, &counter)
+}
+
+func helper2(root *BSTNode, k int, counter *int) *BSTNode {
+	if root == nil {
+		return nil
+	}
+
+	left := helper2(root.left, k, counter)
+	if left != nil {
+		return left
+	}
+	*counter += 1
+	if *counter == k {
+		return root
+	}
+	return helper2(root.right, k, counter)
+}
+
+func RangePrinter(root *BSTNode, K1, K2 int) {
+	if root == nil {
+		return
+	}
+
+	if root.data >= K1 {
+		RangePrinter(root.left, K1, K2)
+	}
+	if root.data >= K1 && root.data <= K2 {
+		fmt.Print(" ", root.data)
+	}
+	if root.data <= K2 {
+		RangePrinter(root.right, K1, K2)
+	}
+}
+
+func RangePrinterWithQ(root *BSTNode, K1, K2 int) {
+	if root == nil {
+		return
+	}
+	var result [][]int
+	queue := []*BSTNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			level = append(level, node.data)
+			queue = queue[1:]
+			if node.data >= K1 && node.data <= K2 {
+				fmt.Print(" ", node.data)
+			}
+			if node.left != nil && node.data >= K1 {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil && node.data <= K2 {
+				queue = append(queue, node.right)
+			}
+		}
+		result = append(result, level)
+	}
+}
+
+func CountTree(n int) int {
+	if n <= 1 {
+		return 1
+	} else {
+		sum := 0
+		for root := 1; root <= n; root++ {
+			left := CountTree(root - 1)
+			right := CountTree(n - root)
+			sum += left * right
+		}
+		return sum
+	}
 }
