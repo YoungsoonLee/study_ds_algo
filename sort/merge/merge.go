e9a3e619dea61704de45bd8523d3f6d21f39367c
Ted Youngsoon Lee <youngtip@gmail.com>
2020-10-18 19:33:03 -0700

    .

--- sort/merge/merge.go
+++ sort/merge/merge.go
@@ -35,3 +35,32 @@ func merge(left, right []int) []int {
 	}
 	return result
 }
+
+func MergeSortTucker(arr []int) []int {
+	if len(arr) == 1 {
+		return arr
+	}
+	left := MergeSortTucker(arr[:len(arr)/2])
+	right := MergeSortTucker(arr[len(arr)/2:])
+
+	i, j := 0, 0
+	rst := make([]int, 0)
+	for i < len(left) || j < len(right) {
+		if i >= len(left) {
+			rst = append(rst, right[j])
+			j++
+		} else if j >= len(right) {
+			rst = append(rst, left[i])
+			i++
+		} else {
+			if left[i] < right[j] {
+				rst = append(rst, left[i])
+				i++
+			} else {
+				rst = append(rst, left[j])
+				j++
+			}
+		}
+	}
+	return rst
+}
